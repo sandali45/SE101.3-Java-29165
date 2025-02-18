9d305be3dba026d97a9f6ddf95c29c1c2e13910c
sandali45 <139307298+sandali45@users.noreply.github.com>
2025-02-18 22:14:39 +0530

    Q3

---
+++ Whileloop.java
@@ -0,0 +1,17 @@
+public class Whileloop {
+    public static void main(String[] args)
+    {
+    
+    
+    int x=0;
+    while(x<=4)
+    
+    {
+    System.out.println("Executing loop:"+x);
+    x++;
+    }
+    
+    
+    }
+   }
+   
