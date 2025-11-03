f114c1ce768b51c4f184fe692abc7525276554ca
sandali45 <139307298+sandali45@users.noreply.github.com>
2025-11-03 20:25:58 +0530

    Add initial implementations of Java programs including loops and user interaction

---
+++ Others/Rectangle_area.java
@@ -0,0 +1,11 @@
+public class Rectangle_area {
+
+    public Static void main(String[] args) {
+        int length = 5;
+        int width = 10;
+        int area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+    
+}
---
+++ Others/Rev1.java
@@ -0,0 +1,11 @@
+
+
+public class Rev1 {
+
+    public static void main(String[] args) {
+        String x = "John ";
+String y = "Doe";
+
+System.out.println( x + y );
+    }
+}
